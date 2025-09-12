c6da3969d5b544e2d3f0e366a5f8ef347d0109e6
Biswajeet Raut <85820671+BiswajeetRaut@users.noreply.github.com>
2025-09-13 03:41:32 +0530

    Update utils.py

--- libs/utils/utils.py
+++ libs/utils/utils.py
@@ -17,5 +17,13 @@ print(my_list)
 
 
 
+my_list = [3, 1, 4, 1, 5, 9, 2]
+my_list.sort()  # Sorts in ascending order by default
+print(my_list)
+# Output: [1, 1, 2, 3, 4, 5, 9]
+
+my_list.sort(reverse=True)  # Sorts in descending order
+print(my_list)
+# Output: [9, 5, 4, 3, 2, 1, 1]
 
 
