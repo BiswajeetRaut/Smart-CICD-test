9dbb5c441383ce44ab52173e1fdefb9de64fbf29
Biswajeet Raut <85820671+BiswajeetRaut@users.noreply.github.com>
2025-09-13 03:29:47 +0530

    Update utils.py

--- libs/utils/utils.py
+++ libs/utils/utils.py
@@ -16,20 +16,6 @@ print(my_list)
 # Output: [9, 5, 4, 3, 2, 1, 1]
 
 
-print("hello world")
-print("what is the value")
 
 
-
-print("Changed content")
-
-
-my_list = [3, 1, 4, 1, 5, 9, 2]
-my_list.sort()  # Sorts in ascending order by default
-print(my_list)
-# Output: [1, 1, 2, 3, 4, 5, 9]
-
-my_list.sort(reverse=True)  # Sorts in descending order
-print(my_list)
-# Output: [9, 5, 4, 3, 2, 1, 1]
 
